31dfeb277c02c76ae3475de407da79cf12b6f998
saipawannidadavolu7 <78499491+saipawannidadavolu7@users.noreply.github.com>
2021-02-03 13:33:26 -0500

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RetrieveDataService } from './retrieveData.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+	HttpClientModule
+  ],
+  providers: [RetrieveDataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
